4543b4e43348dee757cc9803b217c6f460184a6a
Oliver Petchey <olliesp07@gmail.com>
2026-03-09 14:41:38 +0000

    Fixed react-testing related bug and added Bootstrap CDN HTML tags

--- mybase/views.py
+++ mybase/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 
 def index(request):
-    context_dict = {
-        "react_app_source": settings.REACT_MAIN_PATH
-    }
-    return render(request, 'mybase/base.html', context=context_dict)
+    return render(request, 'mybase/base.html', context={})
