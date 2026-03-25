723ab911fe2a92fc54469df53f838855fce81242
Oliver Petchey <olliesp07@gmail.com>
2026-03-25 14:26:42 +0000

    Added site mapping for the home page and search page - however not integrated search functionality with db yet

--- mybase/urls.py
+++ mybase/urls.py
@@ -4,6 +4,7 @@ from mybase import views
 app_name = 'mybase'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('search/', views.search, name='search'),
     path('apis/', views.api_handler, name='api_handler'),
 ]
--- mybase/views.py
+++ mybase/views.py
@@ -3,11 +3,17 @@ from django.conf import settings
 
 from .apis.api_handler import ApiHandler
 
-def index(request):
+def home(request):
     context_dict = {
         "static_css_path": settings.STATIC_CSS_URL
     }
-    return render(request, 'mybase/base.html', context=context_dict)
+    return render(request, 'mybase/home.html', context=context_dict)
+
+def search(request):
+    context_dict = {
+        "query": request.GET.get("q", None)
+    }
+    return render(request, 'mybase/search.html', context=context_dict)
 
 def api_handler(request):
     return ApiHandler.handleReq(request)
