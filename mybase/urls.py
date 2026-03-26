86ecd9c4d4deb7e16ac45b1c7d3d4e8acdb76b88
Oliver Petchey <olliesp07@gmail.com>
2026-03-26 01:57:45 +0000

    Fixed some url mapping issues

--- mybase/urls.py
+++ mybase/urls.py
@@ -6,7 +6,5 @@ app_name = 'mybase'
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search, name='search'),
-    path('sign_up/', views.sign_up, name='sign_up'),
     path('apis/', views.api_handler, name='api_handler'),
-    path('login/', views.user_login, name='login'),
 ]
